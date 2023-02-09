b821f5b765c9d80514e53f13d58bea1089b03f40
ding lan <dinglan@github.com>
2023-02-09 16:49:52 +0800

    项目状态管理redux配置（一）

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
 import "normalize.css"
 import "./assets/css/index.less"
+import store from './store';
 /* 
   craco设置别名 因找文件（../）比较麻烦
   @ => src:webpack
@@ -20,9 +22,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Suspense fallback="loading">
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <Provider store={store}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </Provider>
     </Suspense>
   </React.StrictMode>
 );
