ccc8ff5369364bae6bbbdbae35fea65235835e67
ding lan <dinglan@github.com>
2023-02-12 22:50:33 +0800

    整体App架构和Header不同实现方案

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React, { memo } from 'react'
 import { useRoutes } from 'react-router-dom'
+import AppFooter from './components/app-footer'
+import AppHeader from './components/app-hander'
 import routes from './router'
 
 
 const App = memo(() => {
   return (
     <div className='app'>
-      <div className="header">header</div>
-        <div className="page">
-          {useRoutes(routes)}
-        </div>
-      <div className='footer'>footer</div>
+      <AppHeader/>
+      <div className="page">
+        {useRoutes(routes)}
+      </div>
+      <AppFooter/>
     </div>
   )
 })
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,32 +1,14 @@
-import React, { memo, useEffect, useState } from 'react'
-import hyRequest from '@/services'
+import React, { memo } from 'react'
+
 
 const Home = memo(() => {
 
-  //定义状态
-  const [ highScore, setHighScore ] = useState({})
-
-  //网络请求的代码
-  useEffect(() => {
-    hyRequest.get({ url: "/home/highscore" }).then(res => {
-      console.log(res)
-      setHighScore(res)
-    })
-  }, [])
-
   return (
     <div>
-      <h2>{ highScore.title }</h2>
-      <h4>{ highScore.subtitle}</h4>
-      <ul>
-        {
-          highScore?.list?.map((item) => {//?.可选链 有值才会执行，没值返回undefined
-            return <li key={item.id}>{item.name}</li>
-          })
-        }
-      </ul>
+      <h2>Home Page</h2>
     </div>
   )
+
 })
 
 export default Home
