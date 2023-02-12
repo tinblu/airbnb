053e7e6ef27e0a15816faf5950d5f617b8ee26e3
ding lan <dinglan@github.com>
2023-02-12 15:49:11 +0800

    网络请求axios的封装和测试

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Suspense fallback="loading">
       <Provider store={store}>
         <HashRouter>
@@ -28,7 +28,7 @@ root.render(
         </HashRouter>
       </Provider>
     </Suspense>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,8 +1,31 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState } from 'react'
+import hyRequest from '@/services'
 
 const Home = memo(() => {
+
+  //定义状态
+  const [ highScore, setHighScore ] = useState({})
+
+  //网络请求的代码
+  useEffect(() => {
+    hyRequest.get({ url: "/home/highscore" }).then(res => {
+      console.log(res)
+      setHighScore(res)
+    })
+  }, [])
+
   return (
-    <div>Home</div>
+    <div>
+      <h2>{ highScore.title }</h2>
+      <h4>{ highScore.subtitle}</h4>
+      <ul>
+        {
+          highScore?.list?.map((item) => {//?.可选链 有值才会执行，没值返回undefined
+            return <li key={item.id}>{item.name}</li>
+          })
+        }
+      </ul>
+    </div>
   )
 })
 
