6e68819c889d3bbe4cd3405dd7fd9e3fb26cb402
ding lan <dinglan@github.com>
2023-02-14 21:37:49 +0800

    Header的右侧布局和内容展示

--- src/components/app-hander/c-cpns/header-right/index.jsx
+++ src/components/app-hander/c-cpns/header-right/index.jsx
@@ -1,9 +1,25 @@
+import IconAvatar from '@/assets/svg/icon_avatar'
+import IconGlobal from '@/assets/svg/icon_global'
+import IconMenu from '@/assets/svg/icon_menu'
 import React, { memo } from 'react'
 import { RightWrapper } from './style'
 
 const HeaderRight = memo(() => {
   return (
-    <RightWrapper>HeaderRight</RightWrapper>
+    <RightWrapper>
+      <div className="btns">
+        <span className='btn'>登录</span>
+        <span className='btn'>注册</span>
+        <span className='btn'>
+          <IconGlobal/>
+        </span>
+
+        <div className="profile">
+          <IconMenu/>
+          <IconAvatar/>
+        </div>
+      </div>
+    </RightWrapper>
   )
 })
 
