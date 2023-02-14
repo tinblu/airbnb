78dad3bed870505c86a9518534ba4b354e2808b6
ding lan <dinglan@github.com>
2023-02-15 00:10:28 +0800

    首页-顶部轮播图图片展示

---
+++ src/views/home/c-cpns/home-banner/index.jsx
@@ -0,0 +1,13 @@
+import React, { memo } from 'react'
+import { BannerWarpper } from './style'
+// import coverImg from '@/assets/img/cover_01.jpeg'
+
+const HomeBanner = memo(() => {
+  return (
+    <BannerWarpper>
+      {/* <img src={coverImg} alt=''/> */}
+    </BannerWarpper>
+  )
+})
+
+export default HomeBanner
