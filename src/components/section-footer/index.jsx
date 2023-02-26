769e8bc79578d55022972958160a1952ab65ae9d
ding lan <dinglan@github.com>
2023-02-26 18:40:06 +0800

    全部页面的跳转和整体思路

--- src/components/section-footer/index.jsx
+++ src/components/section-footer/index.jsx
@@ -1,6 +1,7 @@
 import IconMoreArrow from '@/assets/svg/icon-more-arrow'
 import PropTypes from 'prop-types'
 import React, { memo } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { FooterWrapper } from './style'
 
 const SectionFooter = memo((props) => {
@@ -10,9 +11,15 @@ const SectionFooter = memo((props) => {
   if (name) {
     showMessage = `显示更多${name}房源`
   }
+
+  //事件处理
+  const navigate = useNavigate()
+  function moreCliockHandle() {
+    navigate("/entire")
+  }
   return (
     <FooterWrapper color={name ? "#00848A" : "#000"}>
-      <div className="info">
+      <div className="info" onClick={e => moreCliockHandle()}>
         <span className='text'>{showMessage}</span>
         <IconMoreArrow/>
       </div>
