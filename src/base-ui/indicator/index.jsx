ff1fa23543c53d296fc06222c92d614159718d77
ding lan <dinglan@github.com>
2023-03-01 00:50:17 +0800

    indicator组件封装-左右特殊情况

--- src/base-ui/indicator/index.jsx
+++ src/base-ui/indicator/index.jsx
@@ -15,12 +15,18 @@ const Indicator = memo((props) => {
 
     //2.content的宽度
     const contentWidth = contentRef.current.clientWidth
+    const contentScroll = contentRef.current.scrollWidth
 
-    //3.获取selectIndex要滚动的距离
-    const distance = itemLeft + itemWidth * 0.5 - contentWidth * 0.5
+    //3.获取selectIndex要滚动的距离 //往右边移动是负值
+    let distance = itemLeft + itemWidth * 0.5 - contentWidth * 0.5
     
-    contentRef.current.style.transform = `translate(${-distance}px)`
+    //4.特殊情况的处理
+    if(distance < 0) distance = 0 //左边的特殊处理
+    const totalDistance = contentScroll - contentWidth
+    if(distance > totalDistance) distance = totalDistance //右边的特殊处理
 
+    //改变位置即可
+    contentRef.current.style.transform = `translate(${-distance}px)`
   }, [selectIndex])
   return ( 
     <IndicatorWrapper>
